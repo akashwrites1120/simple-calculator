e1acf94e7425efec81304be21b8a8a0d24706b5f
Akash Maity <akashmaity4452@gmail.com>
2025-10-20 10:48:30 +0530

    basic setup

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Calculator = () => {
+  const keys = [
+    "AC",
+    "C",
+    "%",
+    "/",
+    "7",
+    "8",
+    "9",
+    "*",
+    "4",
+    "5",
+    "6",
+    "-",
+    "1",
+    "2",
+    "3",
+    "+",
+    ".",
+    "0",
+    "EQUALS",
+  ];
+  return <div className="min-h-[320px] bg-black">Calculator</div>;
+};
+
+export default Calculator;
